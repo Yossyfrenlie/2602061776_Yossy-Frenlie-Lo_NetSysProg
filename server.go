52f707f29446514af66a0df9a33ecbc13dbf3fae
Yossyfrenlie <119718711+Yossyfrenlie@users.noreply.github.com>
2024-04-06 21:36:12 +0700

    server.go

--- server.go
+++ server.go
@@ -1,33 +1,52 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9999")
+	listener, err := net.Listen("tcp", "127.0.0.1:9999")
 	if err != nil {
-		return
+		panic(err)
 	}
 	defer listener.Close()
 
 	for {
-		clientConn, err := listener.Accept()
+		clientConnection, err := listener.Accept()
 		if err != nil {
-			return
+			panic(err)
 		}
-		go func(client net.Conn) {
-			fmt.Println("Accepted")
-			buf := make([]byte, 100)
-			n, err := client.Read(buf)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Printf("read: %d, message %s", n, buf)
-		}(clientConn)
-
+		go handleServer(clientConnection)
 	}
+}
 
+func handleServer(client net.Conn) {
+	var size uint32
+	err := binary.Read(client, binary.LittleEndian, &size)
+	if err != nil {
+		panic(err)
+	}
+	bytMsg := make([]byte, size)
+	client.SetReadDeadline(time.Now().Add(15 * time.Second))
+	_, err = client.Read(bytMsg)
+	if err != nil {
+		panic(err)
+	}
+	strMsg := string(bytMsg)
+	fmt.Printf("Received: %s\n", strMsg)
+
+	reply := "Message has been received"
+
+	err = binary.Write(client, binary.LittleEndian, uint32(len(reply)))
+	if err != nil {
+		panic(err)
+	}
+	client.SetWriteDeadline(time.Now().Add(15 * time.Second))
+	_, err = client.Write([]byte(reply))
+	if err != nil {
+		panic(err)
+	}
 }
