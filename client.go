7f907307fb34ead13603d0abb40df7acbf077a97
Yossyfrenlie <119718711+Yossyfrenlie@users.noreply.github.com>
2024-04-06 21:35:17 +0700

    client.go

--- client.go
+++ client.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"bufio"
+	"encoding/binary"
 	"fmt"
 	"net"
+	"os"
+	"time"
 )
 
 func main() {
-	serverConnection, err := net.Dial("tcp", "localhost:9999")
+	scan := bufio.NewScanner(os.Stdin)
+	fmt.Print("Insert Message: ")
+	scan.Scan()
+	message := scan.Text()
 
+	sendToServer(message)
+}
+
+func sendToServer(message string) {
+	serverConnection, err := net.DialTimeout("tcp", "127.0.0.1:9999", 15*time.Second)
 	if err != nil {
-		return
+		panic(err)
 	}
 	defer serverConnection.Close()
 
-	payload := "Hello World!"
-	_, err = serverConnection.Write([]byte(payload))
+	err = binary.Write(serverConnection, binary.LittleEndian, uint32(len(message)))
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
+
+	_, err = serverConnection.Write([]byte(message))
+	if err != nil {
+		panic(err)
+	}
+
+	var size uint32
+	err = binary.Read(serverConnection, binary.LittleEndian, &size)
+	if err != nil {
+		panic(err)
+	}
+
+	byteReply := make([]byte, size)
+	serverConnection.SetReadDeadline(time.Now().Add(15 * time.Second))
+	_, err = serverConnection.Read(byteReply)
+	if err != nil {
+		panic(err)
+	}
+	reply := string(byteReply)
+	fmt.Printf("Replied: %s\n", reply)
 }
